9b8b6b211d894e2abcdff3cade11a33627c060ba
jose-mha <josemhha@gmail.com>
2021-04-26 22:31:01 -0500

    :white_check_mark: Relaciones: pertenencia
    - Prueba agregada para saber si un repositorio pertenece a un usuario, asi como sus respectivas configuraciones para que la prueba pase a verde.

--- database/factories/RepositoryFactory.php
+++ database/factories/RepositoryFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+
 use App\Models\Repository;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -22,7 +24,7 @@ class RepositoryFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'user_id' => User::Factory()
         ];
     }
 }
