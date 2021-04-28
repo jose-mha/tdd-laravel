a04d8dd5715cc9d164c7bac3a828b4defbd2233a
jose-mha <josemhha@gmail.com>
2021-04-28 11:34:55 -0500

    :white_check_mark: Listado personal
    - Listar en una vista todos los repositorios que un usuario ha creado.

--- tests/Feature/Http/Controllers/RepositoryControllerTest.php
+++ tests/Feature/Http/Controllers/RepositoryControllerTest.php
@@ -23,6 +23,31 @@ class RepositoryControllerTest extends TestCase
         $this->post('repositories', [])->assertRedirect('login'); //store
     }
 
+    public function test_index_empty()
+    {
+        Repository::factory()->create(); //! user_id = 1
+        $user = User::factory()->create(); //!id = 2
+
+        $this
+            ->actingAs($user)
+            ->get('repositories')
+            ->assertStatus(200)
+            ->assertSee('No hay repositorios creados');
+    }
+
+    public function test_index_with_data()
+    {
+        $user = User::factory()->create(); //!id = 2
+        $repository = Repository::factory()->create(['user_id' => $user->id]); //! user_id = 1
+
+        $this
+            ->actingAs($user)
+            ->get('repositories')
+            ->assertStatus(200)
+            ->assertSee($repository->id)
+            ->assertSee($repository->url);
+    }
+
     public function test_store()
     {
         $data = [
