b3392f488476fa7ffc3d1ca5ff8205d8ea28afd6
jose-mha <josemhha@gmail.com>
2021-04-28 13:32:07 -0500

    :white_check_mark: Formulario de creacion
    - Crear prueba para validar que exista un formulario de creacion, para un repositorio nuevo

--- tests/Feature/Http/Controllers/RepositoryControllerTest.php
+++ tests/Feature/Http/Controllers/RepositoryControllerTest.php
@@ -48,6 +48,16 @@ class RepositoryControllerTest extends TestCase
             ->assertSee($repository->url);
     }
 
+    public function test_create()
+    {
+        $user = User::factory()->create();
+
+        $this
+            ->actingAs($user)
+            ->get('repositories/create')
+            ->assertStatus(200);
+    }
+
     public function test_store()
     {
         $data = [
@@ -203,4 +213,5 @@ class RepositoryControllerTest extends TestCase
             ->get("repositories/$repository->id/edit")
             ->assertStatus(403);
     }
+
 }
